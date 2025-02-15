ea24c1fd5c72a9fec1056228953d601ff8dec495
adiellll4 <adieloosh8@gmail.com>
2025-02-15 15:08:20 +0200

    Update FinalProject.ps1

--- FinalProject.ps1
+++ FinalProject.ps1
@@ -1,3 +1,13 @@
+# # קבל את הערכים שהוזנו כפרמטרים
+# $first_name = $args[0]  # שם פרטי
+# $birth_day = $args[1]    # יום הלידה
+# $birth_month = $args[2]  # חודש הלידה
+
+# # הצג את המידע שהוזן
+# Write-Host ("השם שלך הוא: " + $first_name)
+# Write-Host ("יום הלידה: " + $birth_day)
+# Write-Host ("חודש הלידה: " + $birth_month)
+
 # קבל את הערכים שהוזנו כפרמטרים
 $first_name = $args[0]  # שם פרטי
 $birth_day = $args[1]    # יום הלידה
@@ -7,4 +17,3 @@ $birth_month = $args[2]  # חודש הלידה
 Write-Host ("השם שלך הוא: " + $first_name)
 Write-Host ("יום הלידה: " + $birth_day)
 Write-Host ("חודש הלידה: " + $birth_month)
-
