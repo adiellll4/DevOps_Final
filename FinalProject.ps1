36c6f02616253f8eebf75dbcbfd8a7d748eac6b1
adiellll4 <adieloosh8@gmail.com>
2025-02-15 15:06:18 +0200

    Update FinalProject.ps1

--- FinalProject.ps1
+++ FinalProject.ps1
@@ -4,6 +4,7 @@ $birth_day = $args[1]    # יום הלידה
 $birth_month = $args[2]  # חודש הלידה
 
 # הצג את המידע שהוזן
-Write-Host "השם שלך הוא: $first_name"
-Write-Host "יום הלידה: $birth_day"
-Write-Host "חודש הלידה: $birth_month"
+Write-Host ("השם שלך הוא: " + $first_name)
+Write-Host ("יום הלידה: " + $birth_day)
+Write-Host ("חודש הלידה: " + $birth_month)
+
